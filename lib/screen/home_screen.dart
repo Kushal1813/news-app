e04f6e248c277e04d87d53c5961fac0d80657382
Chadmiya Kushal Ramesh <chadmiyar@DEZ-BLR-MAC020.local>
2024-07-12 15:02:02 +0530

    Secondcommit

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> news = [];
+  bool _isLoading = true;
   @override
   void initState() {
     fetchNews();
@@ -25,51 +26,129 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Flutter News App'),
       ),
-      backgroundColor: Colors.grey,
-      body: ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
-            final article = news[index];
-            final title = article['title'];
-            final description = article['description'];
-            final author = article['source']['name'];
-            final publishDate = article['publishedAt'];
-            final thumbnail = Image.network(article['urlToImage'],
-                loadingBuilder: (BuildContext context, Widget body,
-                    ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return body;
-              }
-              return Center(
-                  child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                    : null,
-              ));
-            });
-            return CustomListItem(
-                thumbnail: thumbnail,
-                title: title,
-                subtitle: description,
-                author: author,
-                publishDate: publishDate);
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-          itemCount: news.length),
+      backgroundColor: Colors.white,
+      body: _isLoading
+          ? Shimmer.fromColors(
+              baseColor: Colors.grey.shade400,
+              highlightColor: Colors.white,
+              child: ListView.separated(
+                  padding: const EdgeInsets.all(16),
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 16),
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 96,
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 96,
+                            width: 96,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Expanded(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              Container(
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 16,
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ))
+                        ],
+                      ),
+                    );
+                  }))
+          : ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                final article = news[index];
+                final title = article['title'];
+                final description = article['description'];
+                final author = article['source']['name'];
+                final publishDate = article['publishedAt'];
+                final thumbnail = Image.network(article['urlToImage'],
+                    loadingBuilder: (BuildContext context, Widget body,
+                        ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return body;
+                  }
+                  return Center(
+                      child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes!
+                        : null,
+                  ));
+                });
+                return CustomListItem(
+                    thumbnail: thumbnail,
+                    title: title,
+                    subtitle: description,
+                    author: author,
+                    publishDate: publishDate);
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+              itemCount: news.length),
     );
   }
 
   void fetchNews() async {
-    print('Okay');
     const url = 'https://mocki.io/v1/a3b52530-9ce3-4d02-b057-e59ef3aab339';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    setState(() {
-      news = json['articles'];
-    });
-    print('done');
+    await Future.delayed(const Duration(seconds: 3));
+    if (response.statusCode == 200) {
+      _isLoading = false;
+      final body = response.body;
+      final json = jsonDecode(body);
+      setState(() {
+        news = json['articles'];
+      });
+    } else {
+      throw Exception('HTTP failed');
+    }
   }
 }
